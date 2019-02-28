b8dcfee26902e96c283efa77c00a9e6caf8a1875
reinoptland <reinoptland@gmail.com>
2019-02-28 11:56:33 +0100

    Setup store, add tests

---
+++ redux-only/index.js
@@ -0,0 +1,35 @@
+const redux = require('redux')
+
+// index.js
+const reducer = (state = 1, action = {}) => {
+    switch(action.type) {
+    case 'ADD_ONE':
+      return state + 1
+    case 'ADD_TWO':
+      return state * 2
+    default:
+      return state
+    }
+  }
+  
+
+const store = redux.createStore(reducer)
+console.log('Initial state of the store', store.getState())
+
+store.subscribe(() => console.log('Next state:', store.getState()))
+
+const action = {
+  type: 'ADD_ONE'
+}
+
+store.dispatch(action)
+
+const actionTwo = {
+  type: 'ADD_TWO'
+}
+
+store.dispatch(actionTwo)
+
+module.exports = { reducer }
+
+
---
+++ redux-only/index.spec.js
@@ -0,0 +1,27 @@
+// index.spec.js
+const {reducer} = require('./index')
+
+test('Initial state should be 1', () => {
+  expect(reducer()).toBe(1)
+})
+
+test('1 + ADD_ONE should be 2', () => {
+  const action = {
+    type: 'ADD_ONE'
+  }
+  expect(reducer(1, action)).toBe(2)
+})
+
+test('1 + ADD_TWO should be 3', () => {
+  const action = {
+    type: 'ADD_TWO'
+  }
+  expect(reducer(1, action)).toBe(3)
+})
+
+test('5 + ADD_TWO should be 7', () => {
+  const action = {
+    type: 'ADD_TWO'
+  }
+  expect(reducer(5, action)).toBe(7)
+})
